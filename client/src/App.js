80a2fe8fe859b3b4c456d23616f372f24cedff4d
guttrids <157908319+guttrids@users.noreply.github.com>
2024-10-27 18:56:03 -0500

    Initialize activity-7

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './App.css';
+import axios from 'axios';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login'
+
+class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data ${error}`);
+      })
+  }
+
+  render() {
+    return (
+      <div className='App'> 
+        <header className='App-header'>
+          <h1>GoodThings</h1>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
