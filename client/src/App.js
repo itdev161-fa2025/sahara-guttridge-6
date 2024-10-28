c82fac61b167a976a5549557acc903e52ebf4b90
Sahara Guttridge <guttrids@gmatc.matc.edu>
2024-10-27 19:53:16 -0500

    update activity 7

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import axios from 'axios';
+import { Routes, Route, Link} from 'react-router-dom';
 
 class App extends React.Component {
   state = {
@@ -23,9 +24,20 @@ class App extends React.Component {
     return (
       <div className='App'> 
         <header className='App-header'>
-          GoodThings
+        <h1> GoodThings</h1> 
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
         </header>
-        {this.state.data}
+        <main>
+          <Routes>
+            <Route path="/" />
+            <Route path="/register" />
+            <Route path="/login" />
+          </Routes>
+        </main>
       </div>
     )
   }
