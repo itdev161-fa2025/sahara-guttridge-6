d8e4e54829b5b2ee6d25968731016c93b93246df
Sahara Guttridge <guttrids@gmatc.matc.edu>
2024-10-27 21:30:25 -0500

    Completed Activity 7

---
+++ client/src/components/Register/Registor.js
@@ -0,0 +1,95 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const Register =() => {
+    const[userData, setUserData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+    });
+    
+    const {name, email, password,passwordConfirm } = userData;
+
+    const onChange = e => {
+        const {name, value} = e.target;
+        setUserData({
+            ...userData,
+            [name]: value
+        })
+    }
+    
+const register = async() => {
+
+    if(password !== passwordConfirm)
+    {
+        console.log('Passwords do not match');
+    }
+    else{
+        const newUser ={
+            name: name,
+            email: email,
+            password: password
+        }
+        try{
+            const config ={
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            const body = JSON.stringify(newUser);
+            const res = await axios.post('https:localhost:5000/api/users', body, config);
+            console.log(res.data);
+    
+    
+        }catch(error)
+                {
+            console.error(error.repsonse.data);
+            return;
+            }
+    
+        }
+    }
+   
+    return(
+        <div>
+            <h2>Register</h2>
+        <div>
+        <input 
+        type = 'text'
+        placeholder ='Email'
+        name ='email'
+        value = {password}
+        onChange={e => onChange(e)}/>
+        </div>
+        <div>
+            <input
+            type='text'
+            placeholder='Password'
+            name='password'
+            value={passwordConfirm}
+            onChange={e => onChange(e)}
+             />
+        </div>
+        <div>
+            <input
+            type='text'
+            placeholder='Confirm Password'
+            name='passwordConfirm'
+            value={passwordConfirm}
+            onChange={e => onChange(e)}
+             />
+        </div>
+        <div>
+            <input
+            />
+        </div>
+        <div>
+            <button onClick={()=> register()}>Register</button>
+        </div>
+        </div>
+    )
+
+}
+export default Register
+
